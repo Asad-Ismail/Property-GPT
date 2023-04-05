f2d57fb1d2554c8fc6e48598761ff115484b74fa
Asad-Ismail <asadismaeel@gmail.com>
2023-04-05 18:13:26 +0000

    updated names

--- property_gpt.py
+++ property_gpt.py
@@ -14,4 +14,5 @@ llm=OpenAI(temperature=0,max_tokens=1000,model_name="gpt-3.5-turbo-0301")
 
 from propertyagent.base import create_property_agent as create_property_agnet
 agent = create_property_agnet(llm, df, verbose=True)
-agent.run("Is 2023 a good time to buy a house in netherland?")
+#agent.run("Is 2023 a good time to buy a house in netherland?")
+agent.run("Which cities are good to buy house in Netherlands in 2023 if I have a budget of 400,000 euros?")

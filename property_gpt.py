624b9e5af99820979a9be5a7dada038c239d2916
Asad-Ismail <asadismaeel@gmail.com>
2023-04-07 12:47:28 +0000

    updated prompts

--- property_gpt.py
+++ property_gpt.py
@@ -15,7 +15,7 @@ llm=OpenAI(temperature=0,max_tokens=1000,model_name="text-davinci-003")
 from propertyagent.base import create_property_agent as create_property_agnet
 agent = create_property_agnet(llm, df, verbose=True)
 #agent.run("Is 2023 a good time to buy a house in netherland?")
-query="Is 2023 a good time to buy a house in netherland? If not when should I buy a house in Netherlands?"
+query="Is it better to rent or buy a house in 2023 in Netherlands I am currently paying 1600 per month for rent and intend to stay Netherlands for 5 more years?"
 #query="Which cities are good to buy house in Netherlands in 2023 if I have a budget of 400,000 euros,have good facilities like schools, universities and low crime rate?"
 print(f"Query is {query}")
 agent.run(query)

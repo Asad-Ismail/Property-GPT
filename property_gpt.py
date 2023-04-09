b46aeb7a306f9674c2e324e942f09fe9c148803e
Asad-Ismail <asadismaeel@gmail.com>
2023-04-09 21:06:32 +0000

    updated query

--- property_gpt.py
+++ property_gpt.py
@@ -14,7 +14,12 @@ llm=OpenAI(temperature=0,max_tokens=1000,model_name="text-davinci-003")
 
 agent = create_property_agnet(llm, df, verbose=True,dateRestrict="m6")
 #agent.run("Is 2023 a good time to buy a house in netherland?")
-query="Is it better to rent or buy a house in 2023 in Netherlands I am currently paying 1600 per month for rent and intend to stay Netherlands for 5 more years?"
+#query="Is it better to rent or buy a house in 2023 in Netherlands I am currently paying 1600 per month for rent and intend to stay Netherlands for 5 more years?"
+query="Can you find me the top 3 best deals properties from listing following are my requirements:\
+\n 1. Should be close to Rotterdam city center or should have good connection to public transport \
+\n 2. Should be less than 400000. \
+\n 3. Should have good energe rating \
+\n 4. Should be built in last 10 years"
 #query="Which cities are good to buy house in Netherlands in 2023 if I have a budget of 400,000 euros,have good facilities like schools, universities and low crime rate?"
 print(f"Query is {query}")
 agent.run(query)
